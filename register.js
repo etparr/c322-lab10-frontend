f792c88f09f939b901d3b1b5581fed832202b5ec
ethanparr <ethanparr8@gmail.com>
2024-04-22 21:39:08 -0400

    .

---
+++ register.js
@@ -0,0 +1,30 @@
+
+
+async function signup() {
+    let email = document.getElementById("email").value;
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let customer = {username: username, password: password, email: email}
+    let request = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(customer)
+      };
+      try {
+        let response = await fetch(getHost() + "/signup", request);
+        if(response.status == 200) {  
+            alert("The registration was successful!")
+            location.href = "login.html";
+
+        } else {
+            console.log(`response status:${response.status}`);            
+            alert("Something went wrong!");
+        }
+      }
+      catch(error) {
+        console.log(error);        
+        alert("Something went wrong!");
+      }    
+}
